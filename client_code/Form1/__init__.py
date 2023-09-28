75867506ec2968e90c998df86245f45a8c5e950e
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-09-28 08:15:44 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,3 +11,13 @@ class Form1(Form1Template):
     self.init_components(**properties)
     self.repeating_panel_1.items =app_tables.ingredients.search()
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    V1 = anvil.server.call('ingredients')
+    print('First Decision = ',V1)
+    
+    pass
+
+
+
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -2,30 +2,86 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import pandas as pd
 import pulp
 
 @anvil.server.callable
-def ingredients(Car_A_profit,Car_B_profit):
+def ingredients():
       # Instantiate our problem class
       
       model = pulp.LpProblem("Cost_minimising_blending_problem", pulp.LpMinimize)
   
-      A = pulp.LpVariable('A', lowBound=0, cat='Integer')
-      B = pulp.LpVariable('B', lowBound=0, cat='Integer')
+# Construct our decision variable lists
+      sausage_types = ['economy', 'premium']
+      ingredients = ['pork', 'wheat', 'starch']
+  
+      # Each of these decision variables will have similar characteristics
+      # (lower bound of 0, continuous variables). Therefore we can use PuLP’s LpVariable object’s dict functionality, we can provide our tuple indices.
+        # 6 decision variables
+      ing_weight = pulp.LpVariable.dicts("weight kg",
+                                          ((i, j) for i in sausage_types for j in ingredients),
+                                          lowBound=0,
+                                          cat='Continuous')
       
-      # Objective function
-      model += Car_A_profit * A + Car_B_profit * B, "Profit"
-      
+      # Objective Function
+      model += (pulp.lpSum([
+                    4.32 * ing_weight[(i, 'pork')]
+                    + 2.46 * ing_weight[(i, 'wheat')]
+                    + 1.86 * ing_weight[(i, 'starch')]
+                    for i in sausage_types]))   
+               
       # Constraints
-      model += 3 * A + 4 * B <= 30
-      model += 5 * A + 6 * B <= 60
-      model += 1.5 * A + 3 * B <= 21
+      # 350 economy and 500 premium sausages at 0.05 kg i.e. 50g
+      model += pulp.lpSum([ing_weight['economy', j] for j in ingredients]) == 350 * 0.05
+      model += pulp.lpSum([ing_weight['premium', j] for j in ingredients]) == 500 * 0.05
       
+      # Economy has >= 40% pork, premium >= 60% pork
+      model += ing_weight['economy', 'pork'] >= (
+          0.4 * pulp.lpSum([ing_weight['economy', j] for j in ingredients]))
+      model += ing_weight['premium', 'pork'] >= (
+          0.6 * pulp.lpSum([ing_weight['premium', j] for j in ingredients]))
+      
+      # Sausages must be <= 25% starch
+      model += ing_weight['economy', 'starch'] <= (
+          0.25 * pulp.lpSum([ing_weight['economy', j] for j in ingredients]))
+      model += ing_weight['premium', 'starch'] <= (
+          0.25 * pulp.lpSum([ing_weight['premium', j] for j in ingredients]))
+      
+      # We have at most 30 kg of pork, 20 kg of wheat and 17 kg of starch available
+      model += pulp.lpSum([ing_weight[i, 'pork'] for i in sausage_types]) <= 30
+      model += pulp.lpSum([ing_weight[i, 'wheat'] for i in sausage_types]) <= 20
+      model += pulp.lpSum([ing_weight[i, 'starch'] for i in sausage_types]) <= 17
+      
+      # We have at least 23 kg of pork to use up
+      model += pulp.lpSum([ing_weight[i, 'pork'] for i in sausage_types]) >= 23
+            
       # Solve our problem
       model.solve()
       pulp.LpStatus[model.status]
-      
-      # Print our decision variable values
-      print ('Production of Car A =  ', format(A.varValue))
-      print ( "Production of Car B = {}".format(B.varValue))
-      return A.varValue, B.varValue, pulp.value(model.objective)
+
+  # print decision variables
+      decisions= []
+      for var in ing_weight:
+          var_value = ing_weight[var].varValue
+          print ("The weight of {0} in {1} sausages is {2} kg".format(var[1], var[0], var_value))
+          
+          print(ing_weight[var].varValue)
+          decisions.append(ing_weight[var].varValue)
+           
+          
+  
+      total_cost = pulp.value(model.objective)
+
+      print ("The total cost is €{} for 350 economy sausages and 500 premium sausages".format(round(total_cost, 2)))
+
+      # Printing keys and values separately
+      print("Keys:", list(ing_weight.keys()))
+      print("Values:", list(ing_weight.values()))
+      # Printing a dictionary using a loop and the items() method
+     
+      for key, value in ing_weight.items():
+          print(key, ":", value)
+
+      decisions = pd.DataFrame([ing_weight])
+      print (decisions)
+      return decisions
