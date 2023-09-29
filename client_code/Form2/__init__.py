759d9c5c86e50367f2b03368c4e9de40295ef409
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-09-29 17:22:43 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -11,7 +11,7 @@ class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    scenarios = app_tables.scenario.search()
     # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
@@ -20,13 +20,27 @@ class Form2(Form2Template):
     Sys_Int = self.Sys_Int.text
     Sys_Sys = self.Sys_Sys.text
     Sys_Ins= self.Sys_Ins.text
-    print(Sys_PR + ' ' + Sys_Int + ' ' + Sys_Sys + ' ' + Sys_Ins)
-    
+    print(str(Sys_PR) + ' ' + str(Sys_Int) + ' ' + str(Sys_Sys) + ' ' + str(Sys_Ins))
+    SI_PR = self.SI_PR.text
+    SI_Int = self.SI_Int.text
+    SI_Sys = self.SI_Sys.text
+    SI_Ins= self.SI_Ins.text
     
 
-    app_tables..add_row(projects="Systems",
+    app_tables.days_effort.add_row(projects= "Systems",
                           PreReqs = Sys_PR,
-                          Interfacing = Sys_int)
+                          Interfacing = Sys_Int,
+                          System_config= Sys_Sys,
+                          Installing = Sys_Ins)
                        
+    app_tables.days_effort.add_row(projects= "Standalone Systems",
+                          PreReqs = SI_PR,
+                          Interfacing = SI_Int,
+                          System_config= SI_Sys,
+                          Installing = SI_Ins)
+
+  def scenario_dropdown_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
 
 
