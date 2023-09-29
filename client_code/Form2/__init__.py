4a50e37e7d0239b657e8c3291d51157de18e65dc
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-09-29 16:16:03 +0000

    Edited settings and Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -13,3 +13,20 @@ class Form2(Form2Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    Sys_PR = self.Sys_PR.text
+    Sys_Int = self.Sys_Int.text
+    Sys_Sys = self.Sys_Sys.text
+    Sys_Ins= self.Sys_Ins.text
+    print(Sys_PR + ' ' + Sys_Int + ' ' + Sys_Sys + ' ' + Sys_Ins)
+    
+    
+
+    app_tables..add_row(projects="Systems",
+                          PreReqs = Sys_PR,
+                          Interfacing = Sys_int)
+                       
+
+
