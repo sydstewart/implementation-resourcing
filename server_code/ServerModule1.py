4979ecb3ff5fc9fdc19f56994ff74c3008744092
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-10-01 10:14:20 +0000

    Edited ServerModule1 and Form2

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -182,10 +182,10 @@ def calculate_projects(Scenario):
   #Objective Function
   #get data
   systems_selling_price = app_tables.selling_prices.get(projects='Systems',Scenario=Scenario)
-  # for row in systems_selling_price:
+  # for row in systems_selling_+price:
   print(systems_selling_price['Selling_price'])
   Systems_Selling_Price = systems_selling_price['Selling_price']
-  
+
   systems_selling_price = app_tables.selling_prices.get(projects='Standalone Interfaces',Scenario=Scenario)
   # for row in systems_selling_price:
   print(systems_selling_price['Selling_price'])
@@ -207,3 +207,70 @@ def calculate_projects(Scenario):
   # D = 1
   model += Systems_Selling_Price*A + Standalone_Interfaces_Selling_Price*B + Server_Moves_Selling_Price*C + Upgrades_Selling_Price*D  #'Objective Function'
   # print('Problem', m)
+
+#Constraints
+  print('Days Effort per  project')
+  print('Systems days')
+  system_days = app_tables.days_effort.get(projects='Systems',Scenario=Scenario)
+  print(system_days['PreReqs'],system_days['Interfacing'],system_days['System_config'],system_days['Installing'])
+  print('Standalone Interface days')
+  interface_days = app_tables.days_effort.get(projects='Standalone Interfaces',Scenario=Scenario)
+  print(interface_days['PreReqs'],interface_days['Interfacing'],interface_days['System_config'],interface_days['Installing'])
+  print ('Server Move days')
+  server_move_days = app_tables.days_effort.get(projects='Server Moves',Scenario=Scenario)
+  print(server_move_days['PreReqs'], server_move_days['Interfacing'], server_move_days['System_config'],server_move_days['Installing'])
+  print('Upgrade  days')
+  upgrade_days = app_tables.days_effort.get(projects='Upgrades',Scenario=Scenario)
+  print(upgrade_days['PreReqs'], upgrade_days['Interfacing'], upgrade_days['System_config'],upgrade_days['Installing'])
+
+  pr_constraint_days = app_tables.constraints.get(Resource='PreReqs',Scenario=Scenario)
+  print('Pre Req Constraint Days pa =', pr_constraint_days['Constraint'])
+  int_constraint_days = app_tables.constraints.get(Resource='Interfacing',Scenario=Scenario)
+  print('Interfacing Constraint Days pa =',int_constraint_days['Constraint'])
+  sys_constraint_days = app_tables.constraints.get(Resource='Systems_config',Scenario=Scenario)
+  print('System Config Constraint Days pa =',sys_constraint_days['Constraint'])
+  install_constraint_days = app_tables.constraints.get(Resource='Installing',Scenario=Scenario)
+  print('Installing Constraint Days pa =',install_constraint_days['Constraint'])
+
+# Create constraints
+
+  model += system_days['PreReqs'] * A + interface_days['PreReqs']* B + server_move_days['PreReqs'] * C + upgrade_days['PreReqs'] * D <= pr_constraint_days['Constraint']  # "PreReqs"
+  model += system_days['Interfacing'] * A + interface_days['Interfacing']* B + server_move_days['Interfacing'] * C + upgrade_days['Interfacing'] * D <= int_constraint_days['Constraint'] # "Interfacing"
+  model += system_days['System_config'] * A + interface_days['System_config']* B + server_move_days['System_config'] * C + upgrade_days['System_config'] * D <= sys_constraint_days['Constraint'] # "Systems_config"
+  model += system_days['Installing'] * A + interface_days['Installing']* B + server_move_days['Installing'] * C + upgrade_days['Installing'] * D <= install_constraint_days['Constraint'] # "Installing"
+  D <= 15
+  
+# The problem is solved using PuLP's choice of Solver
+  model.solve()
+  
+
+# Each of the variables is printed with it's resolved optimum value
+  for v in model.variables():
+      print(v.name, "=", v.varValue)
+  print("Total Sales: ", value(model.objective))
+  no_of_systems= model.variablesDict()['Systems'].value()
+  print('No of systems = ',no_of_systems)
+  no_of_standalone_interfaces= model.variablesDict()['Standalone_Interfaces'].value()
+  print('No_of_standalone_interfaces = ', no_of_standalone_interfaces)
+  no_of_server_moves= model.variablesDict()['Server_Moves'].value()
+  print('No_of_server_moves = ',no_of_server_moves)
+  no_of_upgrades= model.variablesDict()['Upgrades'].value()
+  print('No_of_upgrades = ',no_of_upgrades)
+  
+  total_pre_days_used = (system_days['PreReqs'] * no_of_systems + interface_days['PreReqs']* no_of_standalone_interfaces + server_move_days['PreReqs'] * no_of_server_moves +  \
+    upgrade_days['PreReqs'] * no_of_upgrades )
+  print('No of Pre Req days used',total_pre_days_used )
+  
+  total_interfacing_days_used = system_days['Interfacing'] * no_of_systems + interface_days['Interfacing']* no_of_standalone_interfaces + server_move_days['Interfacing'] * \
+   no_of_server_moves + upgrade_days['Interfacing'] * no_of_upgrades
+  print('No of Interfacing days used',total_interfacing_days_used )
+  
+  total_system_config_days_used = system_days['System_config'] * no_of_systems + interface_days['System_config']* no_of_standalone_interfaces+ server_move_days['System_config'] * n+ upgrade_days['System_config'] * D 
+  print('No of System_Config_days used',total_system_config_days_used )
+  
+  
+  total_installing_days_used = system_days['Installing'] * no_of_systems + interface_days['Installing']* no_of_standalone_interfaces + server_move_days['Installing'] * no_of_server_moves + \
+    upgrade_days['Installing'] * no_of_upgrades
+  print('No of Installing_days used',total_installing_days_used )
+  
+  # return value(model.objective),
